cc1a6cfcb2890d966cb1ece5ababf78c75c3f48f
juanfope <157375092+juanfope@users.noreply.github.com>
2024-03-15 17:55:36 -0500

    FilterHalfWorking

--- scripts.js
+++ scripts.js
@@ -1,42 +1,50 @@
 const quoteContainer = document.getElementById('quote-container');
 const moreButton = document.getElementById('more');
+const characterFilterInput = document.getElementById('character-filter');
+let shown = [];
 
-function fetchQuote() {
-    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+function fetchQuotes(characterName = '') {
+    let apiUrl = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=8';
+    if (characterName !== '') {
+        apiUrl += `&character=${encodeURIComponent(characterName)}`;
+    }
+
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const { quote, character, image } = data[0];
-
-            const quoteElement = document.createElement('div');
-            quoteElement.classList.add('quote');
-
-            const characterImage = document.createElement('img');
-            characterImage.src = image;
-            characterImage.alt = character;
-
-            const quoteText = document.createElement('p');
-            quoteText.textContent = `"${quote}" - ${character}`;
-
-            quoteElement.appendChild(characterImage);
-            quoteElement.appendChild(quoteText);
-
             quoteContainer.innerHTML = '';
-            quoteContainer.appendChild(quoteElement);
+            data.forEach(quoteData => {
+                const { quote, character, image } = quoteData;
+
+                if(!shown.includes(character)){
+                    shown.push(character);
+
+                const quoteElement = document.createElement('div');
+                quoteElement.classList.add('quote');
+
+                const characterImage = document.createElement('img');
+                characterImage.src = image;
+                characterImage.alt = character;
+
+                const quoteText = document.createElement('p');
+                quoteText.textContent = `"${quote}" - ${character}`;
+
+                quoteElement.appendChild(characterImage);
+                quoteElement.appendChild(quoteText);
+
+                quoteContainer.appendChild(quoteElement);
+                }
+            });
         })
-        .catch(error => console.error('Error fetching quote:', error));
+        .catch(error => console.error('Error fetching quotes:', error));
 }
 
-function fetchNewCharacter() {
-    fetchQuote();
+function handleFilterChange() {
+    const characterName = characterFilterInput.value.trim();
+    fetchQuotes(characterName);
 }
 
-function simpsons(){
-    for(let i=0; i<8; i++){
-        fetchQuote();
-    }
-}
+moreButton.addEventListener('click', fetchQuotes);
+characterFilterInput.addEventListener('input', handleFilterChange);
 
-moreButton.addEventListener('click', fetchNewCharacter);
-fetchQuote();
-simpsons ();
-
+fetchQuotes();
